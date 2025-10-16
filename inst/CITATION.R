fdbf351d56493abb72c46835b30e4c648352b42d
rlaurentiu <laurentiu.rozylowicz@g.unibuc.ro>
2025-10-16 10:45:51 +0300

    improved citation

--- inst/CITATION.R
+++ inst/CITATION.R
@@ -3,9 +3,9 @@ citHeader("To cite alieninvro in publications use:")
 bibentry(
   bibtype = "Article",
   title   = "From soil to stream and sea: species richness and distribution of alien invertebrates in Romania",
-  author  = "Preda, L., et al.",
+  author  =  person("Cristina", "Preda"), person("Laurentiu", "Rozylowicz"),
   journal = "Neobiota",
-  year    = "2024",
+  year    = "2025",
   note    = "submitted"
 )
 ```
